073a0c486376ba6c2750bbec628b9c5c00f64731
AnnaMescheryakova <anna.egorskaya@gmail.com>
2022-03-22 16:15:16 +0600

    commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,19 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.dropUsersTable();
+        userService.createUsersTable();
+        userService.saveUser("Katya", "Kalinina", (byte) 18);
+        userService.saveUser("Sofia", "Sokolova", (byte) 24);
+        userService.saveUser("Anna", "Egorskaya", (byte) 28);
+        userService.saveUser("Artem", "Myakishev", (byte) 15);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
